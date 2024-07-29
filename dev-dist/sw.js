2e955d68880cf8de52e29c99afddde2f1313cd46
= <=>
2024-07-29 11:31:56 -0300

    Remove SmallHeader

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.abtovss07po"
+    "revision": "0.h79p4n5qjl"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/layouts/LayoutSmall.vue
+++ src/layouts/LayoutSmall.vue
@@ -1,13 +1,10 @@
 <script setup>
-  import SmallHeader from '@/components/templates/small/SmallHeader.vue';
+  //import SmallHeader from '@/components/templates/small/SmallHeader.vue';
   import SmallFooter from '@/components/templates/small/SmallFooter.vue';
 </script>
 
 <template>
   <div id="layout-small">
-    <header>
-      <small-header />
-    </header>
     <main>
       <router-view />
     </main>
